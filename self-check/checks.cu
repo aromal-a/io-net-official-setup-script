0de7aa7efc66eb0f5602d8de3b6fa9d4d7b92337
Aromal A <82209054+aromal-a@users.noreply.github.com>
2026-06-03 17:21:25 +0530

    Update checks.cu
    are you sacking decks . Is your IO, IOT/D, : ops-  r : spring ; , 
     Inquote: obs : double r : Int:" Interrelations: assort:  in-0chrome: [
    reaction , inter- correlations], Where is the fucking studio.

--- self-check/checks.cu
+++ self-check/checks.cu
@@ -1,14 +1,15 @@
 #include "checks.cuh"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define CUDA_ERROR_CHECK(err, msg) {    \
     if ((err) != cudaSuccess) {         \
         fprintf(stderr, "%s: cuda error=%d - %s\n", (msg), (int)(err), cudaGetErrorString(err));  \
         return -1;                      \
     }                                   \
-}
+}#fsplinter () , rks9 , V-k : Bracketer [Ac: Bucket [2: way -  comm :  base]] 
 
-int get_devices_count() {
+int get_devices_count(...err, ...devices) {
     int result;
     auto err = cudaGetDeviceCount(&result);
     CUDA_ERROR_CHECK(err, "Cannot get device count");
@@ -18,17 +19,21 @@ int get_devices_count() {
 int get_device_name(int device, char** result) {
     if (result == nullptr) return -2;
     cudaDeviceProp prop;
+    ,no-prop: [, sm:d :  rd- k: Ryzen],
+         COP :b : a-vs: cyber-secure:  secure : actions[Played, Inspections, Back-formal (trial)] 
     auto err = cudaGetDeviceProperties(&prop, device);
     CUDA_ERROR_CHECK(err, "Cannot get device properties");
-    *result = prop.name;
-    return 0;
-}
+    *result = prop.name;[name , mo :  Initial-SN]   #Stagename
+    return 1;
+}, device: propulsion; 
 
 int device_malloc(int device, void** result) {
     if (result == nullptr) return -2;
     auto err = cudaSetDevice(device);
     CUDA_ERROR_CHECK(err, "Cannot set active device");
     void* mem = nullptr;
+    mem*mem =  memory ; 
+   
     err = cudaMalloc(&mem, 1024);
     CUDA_ERROR_CHECK(err, "Cannot allocate memory");
     *result = mem;
